e39e881026549cb450f8d132fc430c0ab8fa8d3a
torgerbocianowski <torgerboc@gmail.com>
2023-10-09 21:45:55 +0200

    Delete user

--- frontend/app/components/icons.tsx
+++ frontend/app/components/icons.tsx
@@ -82,6 +82,7 @@ export const PlusIcon = ({ size, animation, style }: IconProps) => {
     </div>
   );
 };
+
 export const MinusIcon = ({ size, animation, style }: IconProps) => {
   if (!size) {
     size = 5;
@@ -93,6 +94,22 @@ export const MinusIcon = ({ size, animation, style }: IconProps) => {
     >
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <path d="M5 11V13H19V11H5Z"></path>
+      </svg>
+    </div>
+  );
+};
+
+export const TrashIcon = ({ size, animation, style }: IconProps) => {
+  if (!size) {
+    size = 5;
+  }
+
+  return (
+    <div
+      className={`h-${size} w-${size} m-1 ${style} hover:animate-${animation}`}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"></path>
       </svg>
     </div>
   );
--- frontend/app/routes/users._index.tsx
+++ frontend/app/routes/users._index.tsx
@@ -1,7 +1,40 @@
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { type ActionFunction, json } from "@remix-run/node";
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import { useState } from "react";
 import { styles } from "~/common/styles";
 import { type User } from "~/common/types";
+import { TrashIcon } from "~/components/icons";
+
+export const action: ActionFunction = async ({ request }) => {
+  const body = await request.formData();
+  const id = body.get("id")?.toString();
+  const token = body.get("token");
+  if (!token) {
+    return new Response("Missing token", {
+      status: 400,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+  }
+  const response = await fetch(`http://localhost:8080/api/users/${id}`, {
+    method: "DELETE",
+    headers: {
+      "x-jwt-token": token.toString(),
+    },
+  });
+
+  if (response.status !== 200) {
+    return new Response("Something went wrong", {
+      status: response.status,
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+  }
+
+  return json({ message: "success" }, { status: 200 });
+};
 
 export const loader = async () => {
   const res = await fetch("http://localhost:8080/api/users");
@@ -10,6 +43,7 @@ export const loader = async () => {
 
 export default function Accounts() {
   const users = useLoaderData<typeof loader>();
+  const [showDelete, setShowDelete] = useState(false);
   return (
     <main className="">
       <h1 className="font-bold text-4xl ">Users</h1>
@@ -24,10 +58,28 @@ export default function Accounts() {
           </div>
         ))}
       {users && users.length > 0 && (
-        <ul>
+        <ul className="flex flex-col gap-2">
           {users.map((user: User) => (
-            <li key={user.id}>
-              {user.firstName} {user.lastName}
+            <li
+              key={user.id}
+              className="grid grid-cols-2"
+              onClick={() => setShowDelete(true)}
+            >
+              <p className="">
+                {user.firstName} {user.lastName}
+              </p>
+              {showDelete && (
+                <Form action="/users" method="DELETE">
+                  <input type="hidden" name="id" value={user.id} />
+                  <input type="hidden" name="token" value={user.token} />
+                  <button
+                    className="h-fit w-fit"
+                    onClick={() => setShowDelete(false)}
+                  >
+                    <TrashIcon />
+                  </button>
+                </Form>
+              )}
             </li>
           ))}
         </ul>
