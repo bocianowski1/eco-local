f597c5027cf63599e3beb853b39eaac499b1b629
torgerbocianowski <torgerboc@gmail.com>
2023-10-12 15:33:53 +0200

    TODO: compose.yml

--- functions/recommender/recommend/__init__.py
+++ functions/recommender/recommend/__init__.py
@@ -3,15 +3,15 @@ import logging
 
 import azure.functions as func
 
-def json_res(message: str, status_code: int = 200, error = None) -> func.HttpResponse:
+def json_res(message: str, status_code: int = 200, error: Exception = None) -> func.HttpResponse:
     body = {
         "status_code": status_code,
         "message": message,
-        "error": json.dumps(error.__dict__) if error else None
+        "error": str(error) if error else None,
     }
     return func.HttpResponse(
         body=json.dumps(body),
-        status_code=200,
+        status_code=status_code,
     )
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -19,9 +19,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     func.HttpResponse.mimetype = "application/json"
 
     try:
+        if req.method != "GET":
+            return json_res(f"{req.method} method not allowed", 405)
         return json_res("Hello from the recommend function!")
         
     except Exception as e:
-        logging.info("\nAn error occurred\n")
         logging.error(e)
         return json_res("An error occurred", 500, e)
